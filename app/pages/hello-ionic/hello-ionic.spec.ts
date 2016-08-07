4e6baab327779aae59419045e9813be7be263d23
Iker Mariñelarena <iker@quinientoscuarenta.com>
2016-08-07 21:26:13 +0200

    [] - YourTurn birth

---
+++ app/pages/hello-ionic/hello-ionic.spec.ts
@@ -0,0 +1,9 @@
+import {beforeEachProviders, it, describe, expect, inject} from '@angular/core/testing';
+import {HelloIonicPage} from './hello-ionic';
+
+describe('First test', () => {
+
+  it('should do nothing', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ app/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,10 @@
+import {Page} from 'ionic-angular';
+
+@Page({
+  templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
+})
+export class HelloIonicPage {
+  constructor() {
+
+  }
+}
